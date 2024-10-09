7ff7ba605e55da6d5a1e7ce29c94a7faf521ad8f
Lưu Xuân Hòa (HN-BCD-Developer) <hoalx@runsystem.local>
2024-10-09 17:40:43 +0700

    1

--- addons_custom/crm_custom/__manifest__.py
+++ addons_custom/crm_custom/__manifest__.py
@@ -1,9 +1,11 @@
 {
     'name': 'CRM Custom',
     'version': '1.0',
-    'depends': ['crm', 'sale'],
+    'depends': ['crm', 'sale', 'sales_team'],
     'data': [
+        'security/crm_security_inherit.xml',
         # 'views/crm_team_target_modal_views.xml',
         'views/crm_team_inherit_views.xml',
+        
     ],
 }
