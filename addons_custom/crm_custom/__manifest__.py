e686329544603aa1eedaf80d1336f427577dd2db
Lưu Xuân Hòa (HN-BCD-Developer) <hoalx@runsystem.local>
2024-10-22 10:55:50 +0700

    .

--- addons_custom/crm_custom/__manifest__.py
+++ addons_custom/crm_custom/__manifest__.py
@@ -5,15 +5,11 @@
     'data': [
         'security/crm_security_inherit.xml',
         'security/ir.model.access.csv',
-
         # 'views/crm_lead_popup.xml',
-
+        'views/crm_lead_oppor_custom_views.xml',
         'wizard/crm_lead_oppor_filter_views.xml',
         'views/crm_team_inherit_views.xml',
-        'views/crm_lead_inherit_views.xml', 
+        'views/crm_lead_inherit_views.xml',
         'views/assets.xml',
-
-
     ],
-  
 }
--- addons_custom/crm_custom/wizard/crm_lead_oppor_filter.py
+++ addons_custom/crm_custom/wizard/crm_lead_oppor_filter.py
@@ -6,6 +6,9 @@ from dateutil.relativedelta import relativedelta
 class CrmLeadOpporFilter(models.TransientModel):
     _name = 'crm.lead.oppor.filter'
     _description = 'Filter Opportunities from Leads'
+    year = fields.Selection([
+        (str(i), f"Năm {i}") for i in range(2020, 2031)
+    ], string="Năm", required=True)
 
     month = fields.Selection([
         (str(i), f"tháng {i}") for i in range(1, 13)
@@ -13,18 +16,21 @@ class CrmLeadOpporFilter(models.TransientModel):
 
     def action_filter_oppor(self):
         selected_month = int(self.month)
-        start_date = datetime(datetime.today().year, selected_month, 1)
-        end_date = start_date + relativedelta(months=1)
+        selected_year = int(self.year)
+        start_date = datetime(selected_year, selected_month, 1)
+        end_date = start_date + relativedelta(months=1, days=-1)
         oppors = self.env['crm.lead'].search([
             ('create_date', '>=', start_date),
-            ('create_date', '<', end_date)
+            ('create_date', '<=', end_date),
+
         ])
+
         return {
             'name': 'Opportunities from Leads',
             'type': 'ir.actions.act_window',
             'res_model': 'crm.lead',
             'view_mode': 'tree',
 
-            'views': [(self.env.ref('crm.crm_case_tree_view_oppor').id, 'tree')],
+            'views': [(self.env.ref('crm_custom.crm_lead_oppor_custom_views').id, 'tree')],
             'domain': [('id', 'in', oppors.ids)],
         }
